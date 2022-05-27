b6c44c207996e3154d869310721e2d0552e63d53
Prakash Venkiteswaran <pvenkiteswara@guidewire.com>
2022-05-27 16:12:13 +0530

    add copy func

--- common.go
+++ common.go
@@ -3,3 +3,19 @@ package testpackage
 func AddIntNum(num1, num2 int) int {
   return num1 + num2
 }
+
+func copyFile(srcName, targetName string) {
+
+
+    bytesRead, err := ioutil.ReadFile(srcName)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = ioutil.WriteFile(targetName, bytesRead, 0644)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+}
